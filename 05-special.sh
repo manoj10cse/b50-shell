43119906b63fbc6fc0d0df1cd0987a264968d434
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-05 16:40:56 +0530

    asdfg

---
+++ 05-special.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# special variables 
+
+#  $0          : This will gives the name of the script you're running 
+#  $1 to $9    :You can pass a maximum of 9 variables from the command line when you're running the script 
+#  $*          : Prints you all the supplied variables in the script 
+#  $@          : Prints you all the supplied variables in the script 
+#  $#          : Prints you the number of variables
+#  $$          : Process ID of the script that you're running 
+#  $?          : Gives you the exit code of the previous command
+
+echo -e "script name that you're running"
+
+a=10 
+b=$1      # $1 first argement passed to the script 
+c=$2 
+d=$3 
+echo value of a is : $a 
+echo value of b is : $b 
+echo value of c is : $c 
