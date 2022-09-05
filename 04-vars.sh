c34812252a8faa05594cb4ba5c44939416f76409
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-05 16:15:25 +0530

    asdfg

--- 04-vars.sh
+++ 04-vars.sh
@@ -13,3 +13,11 @@ echo ${a}
 echo "${b}"
 
 echo value of f is : $f
+
+# rm -rf  /data/${DIR}  
+# rm -rf /data/
+# Above have not declared the dir so it will delete the /data...
+
+echo "Good Morning Santhosh, Todays date is $DATE_COMMAND"
+# This is how we can fetch the system data
+echo "Number of logged in users are: $LOGGEDIN_USERS"
