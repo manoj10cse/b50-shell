8a3d978a5cfb924ff0ef817ec5b658070987168f
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-09 18:41:42 +0530

    new line

---
+++ roboshop/components/common.sh
@@ -0,0 +1,15 @@
+# validating whether the executing user is root or not
+ID=$(id -u)
+if [ $ID -ne 0 ]; then 
+    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
+    exit 1
+fi 
+
+# Declaring the stat function
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m Success \e[0m" 
+    else
+        echo -e "\e[31m Failure. Look for the logs \e[0m"  
+    fi 
+}
