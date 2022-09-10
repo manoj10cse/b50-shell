14bfe6bf6c094e5d338f980892211c4310a50bf8
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-10 16:30:15 +0530

    new line

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Installing the installing mongoDB"
+source components/common.sh
+
+COMPONENT=mongodb
+
+echo -n "Configuring the MongoDB repo:"
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
+stat $? 
+
+echo -n "Installing ${COMPONENT}:"
+yum install -y mongodb-org >> /tmp/${COMPONENT}.log
+stat $? 
+
+systemctl enable mongod >> tmp/${COMPONENT}.log
+systemctl start mongod
+
+echo -n "Updating the $COMPONENT Config:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
+stat $? 
+
+echo -n "Start the $COMPONENT service"
+systemctl enable mongod >> /tmp/${COMPONENT}.log
+systemctl start mongod  
+stat $? 
