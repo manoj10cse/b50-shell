d8835e0226a4ed410b99336efb7e2b3eef8453b3
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-11 12:16:49 +0530

    new line

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -26,9 +26,11 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONE
 stat $?
 
 echo -n "Extract $COMPONENT Schema:"
-cd /tmp && unzip -o mongodb.zip && cd mongodb-main
+cd /tmp && unzip -o mongodb.zip  
 stat $? 
 
 echo -n "Injecting the $COMPONENT schema: "
-mongo < catalogue.js && mongo < users.js 
+cd mongodb-main
+mongo < catalogue.js >> /tmp/${COMPONENT}.log
+mongo < users.js >> /tmp/${COMPONENT}.log
 stat $? 
