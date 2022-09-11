a3a0f214321cbfbea06e3819651c68d55f482734
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-11 17:08:04 +0530

    new line

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -19,7 +19,7 @@ echo -n "Adding $FUSER user:"
 id ${FUSER} >> /tmp/${COMPONENT}.log  || useradd ${FUSER}
 stat $? 
 
-echo -n "Downloading ${COMPONENT} :"
+echo -n "Download ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
 stat $? 
 
@@ -27,16 +27,16 @@ echo -n "Cleanup of Old ${COMPONENT} content:"
 rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
 stat $?
 
-echo -n "Extracting ${COMPONENT} content: "
+echo -n "Extract ${COMPONENT} content: "
 cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
 unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
 stat $? 
 
-echo -n "Changing the ownership to ${FUSER}:"
+echo -n "Change the ownership to ${FUSER}:"
 chown -R $FUSER:$FUSER $COMPONENT/
 stat $?
 
-echo -n "Installing $COMPONENT Dependencies:"
+echo -n "Install $COMPONENT Dependencies:"
 cd $COMPONENT && npm install &>> /tmp/${COMPONENT}.log 
 stat $? 
 
