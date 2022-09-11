d247ffd516f8e8689184d22c97d568976bacb55c
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-11 16:52:23 +0530

    new line

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,52 +1,53 @@
+
 #!/bin/bash
 
-set -e
+set -e 
 
 source components/common.sh
 
 COMPONENT=catalogue
-FUSER=roboshop
+FUSER=roboshop 
 
-echo -n "configure yum repos to nodeJS:"
+echo -n "Configure Yum Remos for nodejs:"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash >> /tmp/${COMPONENT}.log 
 stat $?
 
-echo -n "Install NodejS""
+echo -n "Installing nodejs:"
 yum install nodejs -y >> /tmp/${COMPONENT}.log 
-stat $?
+stat $? 
 
-echo -n "addd user roboshop"
-id ${FUSER} >> /tmp/${COMPONENT}.log  || useradd ${roboshop} 
-stat $?
+echo -n "Adding $FUSER user:"
+id ${FUSER} >> /tmp/${COMPONENT}.log  || useradd ${roboshop}   # Creates users only in case if the user account doen's exist
+stat $? 
 
-echo -n "download $COMPONENT:"
+echo -n "Downloading ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
-stat $?
+stat $? 
 
 echo -n "Cleanup of Old ${COMPONENT} content:"
 rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
 stat $?
 
-echo -n "Extract ${COMPONENT} content: "
+echo -n "Extracting ${COMPONENT} content: "
 cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
 unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
 stat $? 
 
-echo -n "Change ownership to ${FUSER}:"
+echo -n "Changing the ownership to ${FUSER}:"
 chown -R $FUSER:$FUSER $COMPONENT/
 stat $?
 
-echo -n "Install $COMPONENT Dependency:"
+echo -n "Installing $COMPONENT Dependencies:"
 cd $COMPONENT && npm install &>> /tmp/${COMPONENT}.log 
 stat $? 
 
-# echo -n "Config the Systemd file: "
-# sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
-# mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/catalogue.service
-# stat $? 
+echo -n "Configuring the Systemd file: "
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
+mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/catalogue.service
+stat $? 
 
-# echo -n "Starting the service"
-# systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
-# systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
-# systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
-# stat $?
+echo -n "Starting the service"
+systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
+systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
+systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
+stat $?
