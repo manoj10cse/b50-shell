257ba783c11912d2580d4dff92a6399fce32cec7
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-11 16:19:04 +0530

    new line

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,52 @@
 #!/bin/bash
 
-echo "Installing the catalogue"
+set -e
+
+source components/common.sh
+
+COMPONENT=catalogue
+FUSER=roboshop
+
+echo -n "configure yum repos to nodeJS:"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+stat $?
+
+echo -n "Install NodejS""
+yum install nodejs -y
+stat $?
+
+echo -n "addd user roboshop"
+useradd $FUSER
+stat $?
+
+echo -n "download $COMPONENT:"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
+stat $?
+
+echo -n "Cleanup of Old ${COMPONENT} content:"
+rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+stat $?
+
+echo -n "Extract ${COMPONENT} content: "
+cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
+unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
+stat $? 
+
+echo -n "Change the ownership to ${FUSER}:"
+chown -R $FUSER:$FUSER $COMPONENT/
+stat $?
+
+echo -n "Install $COMPONENT Dependencies:"
+cd $COMPONENT && npm install &>> /tmp/${COMPONENT}.log 
+stat $? 
+
+echo -n "Config the Systemd file: "
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
+mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/catalogue.service
+stat $? 
+
+echo -n "Starting the service"
+systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
+systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
+systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
+stat $?
