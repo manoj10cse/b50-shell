cc322d5bfbacb4700447c666cb1270804b315076
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-12 16:05:49 +0530

    new line

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -7,17 +7,9 @@ source components/common.sh
 COMPONENT=catalogue
 FUSER=roboshop 
 
-echo -n "Configure Yum Remos for nodejs:"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >> /tmp/${COMPONENT}.log 
-stat $?
 
-echo -n "Installing nodejs:"
-yum install nodejs -y >> /tmp/${COMPONENT}.log 
-stat $? 
-
-echo -n "Adding $FUSER user:"   
-id ${FUSER} >> /tmp/${COMPONENT}.log  || useradd ${FUSER}
-stat $? 
+NODEJS
+USER_SETUP
 
 echo -n "Download ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -23,3 +23,58 @@ USER_SETUP() {
     id ${FUSER} &>> LOGFILE  || useradd ${FUSER}   
     stat $? 
 }
+
+DOWNLOAD_AND_EXTRACT() {
+    echo -n "Downloading ${COMPONENT} :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
+    stat $? 
+
+    echo -n "Cleanup of Old ${COMPONENT} content:"
+    rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+    stat $?
+
+    echo -n "Extracting ${COMPONENT} content: "
+    cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
+    unzip -o  /tmp/${COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
+    stat $? 
+
+    echo -n "Changing the ownership to ${FUSER}:"
+    chown -R $FUSER:$FUSER $COMPONENT/
+    stat $?
+} 
+
+CONFIG_SVC() {
+    echo -n "Configuring the Systemd file: "
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
+    mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    stat $? 
+
+    echo -n "Starting the service"
+    systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
+    systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
+    systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
+    stat $?
+}
+
+NODEJS() {
+    echo -n "Configure Yum Remos for nodejs:"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash >> /tmp/${COMPONENT}.log 
+    stat $?
+
+    echo -n "Installing nodejs:"
+    yum install nodejs -y >> /tmp/${COMPONENT}.log 
+    stat $? 
+    
+    # Calling User creation function
+    USER_SETUP 
+
+    # Calling Download and extract function
+    DOWNLOAD_AND_EXTRACT
+
+    echo -n "Installing $COMPONENT Dependencies:"
+    cd $COMPONENT && npm install &>> /tmp/${COMPONENT}.log 
+    stat $? 
+    
+    # Calling Config_SVC Function
+    CONFIG_SVC
+}
