7647deb01bbada14eee6e8cf44dc8d4bf4ed8e52
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-15 12:25:41 +0530

    new line

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-# AMI_ID="ami-00ff427d936335825"
+#Ami-id="ami-00ff427d936335825"
 
 
 if [ -z  "$1" ] ; then 
@@ -12,3 +12,7 @@ COMPONENT=$1
 ZONEID="Z04602961I29SHWLCRCU3"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID="sg-000671b0e1fb3d069"
+
+
+#PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
