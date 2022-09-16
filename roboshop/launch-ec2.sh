7afba239cc533cb514727defd108dc8491f851db
santhohsramana96 <patchigolladurgeshh@gmail.com>
2022-09-16 16:19:55 +0530

    new line

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -11,7 +11,7 @@ fi
 COMPONENT=$1
 ZONEID="Z06355883I2R9ZLPV1NHY"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID="Z10155502CWKBH7M5VVE9"
+SGID="sg-08bfd3f1791d7713f"
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g')
 #aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq
